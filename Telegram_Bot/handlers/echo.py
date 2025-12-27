2b7bac7769f1db5114842387b20e879f9da42041
Yasaman-Saffar <154500788+Yasaman-Saffar@users.noreply.github.com>
2025-12-27 12:39:24 -0800

    Add project as subdirectory

---
+++ Telegram_Bot/handlers/echo.py
@@ -0,0 +1,8 @@
+from aiogram import Router
+from aiogram.types import Message
+
+router = Router()
+
+@router.message()
+async def echo_handler(message : Message):
+    await message.answer(f"صداتو اکو میکنم: {message.text}")
